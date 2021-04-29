1117cd47e2468e0819365e7546bba628be132da6
Gumernus <70224036+gumernus@users.noreply.github.com>
2021-04-29 15:35:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
+const express = require('express');
+const server = express();
+var http = require('http');
+var fs = require('fs');
+
+server.all('/', (req, res)=>{
+
+   res.setHeader('Content-Type', 'text/html');
+
+   res.write('<link href="https://fonts.googleapis.com/css?family=Roboto Condensed" rel="stylesheet"> <style> body {font-family: "Roboto Condensed";font-size: 22px;} <p>Hosting Active</p>');
+
+
+   res.end();
+
+})
+
+
+
+function keepAlive(){
+
+   server.listen(3000, ()=>{console.log("Server je online")});
+
+}
+
+module.exports = keepAlive
+
 
