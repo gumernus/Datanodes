27b8ea30dc23727214059eb2fcff8542c9a7c096
Gumernus <70224036+gumernus@users.noreply.github.com>
2021-04-29 15:35:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+const keepAlive = require("./server");
+require("events").EventEmitter.defaultMaxListeners = 15;
+
+
+//commands
+const { helpCommand, helpNormalCommand, helpAdminCommand } = require("./commands/normal/help");
+const {joinCommand, leaveCommand} = require("./commands/normal/join_leave")
+
+client.on("guildMemberAdd", (member) => {
+  joinCommand(member, client, Discord);
+});
+
+client.on("guildMemberRemove", (member) => {
+  leaveCommand(member, client, Discord);
+});
+
+client.on("ready", () => {
+  console.log("The client is ready!");
+  client.user.setStatus("available");
+  client.user.setActivity(config.status);
+
+  client.on("message", (message) => {
+    if (message.content === ".help") {
+      helpCommand(message, Discord);
+    }
+    if (message.content === ".help normal") {
+      helpNormalCommand(message, Discord);
+    }
+    if (message.content === ".help admin") {
+      helpAdminCommand(message, Discord);
+    }
+  });
+});
+
+keepAlive();
+client.login(config.token);
 
