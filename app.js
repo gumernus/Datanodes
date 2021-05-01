34ee2f9fb95d523295a2428052b4f18f18730b25
Gumernus <70224036+gumernus@users.noreply.github.com>
2021-05-01 21:16:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require("events").EventEmitter.defaultMaxListeners = 15;
 const { helpCommand, helpNormalCommand, helpAdminCommand } = require("./commands/normal/help");
 const {joinCommand, leaveCommand} = require("./commands/normal/join_leave")
 const {sayCommand} = require("./commands/normal/easy")
+const {newObj, updateObj, deleteObj} = require("./database/database")
 
 client.on("guildMemberAdd", (member) => {
   joinCommand(member, client, Discord);
@@ -21,24 +22,34 @@ client.on("guildMemberRemove", (member) => {
 client.on("ready", () => {
   console.log("The client is ready!");
   client.user.setStatus("available");
-  client.user.setActivity(config.status);
+  client.user.setActivity(config.prefix + config.status);
 
   client.on("message", (message) => {
     if (message.author.bot) {
       return
     }
     else{
-      if (message.content === ".help") {
+      if (message.content === `${config.prefix}help`) {
         helpCommand(message, Discord);
       }
-      if (message.content === ".help normal") {
+      if (message.content === `${config.prefix}help normal`) {
         helpNormalCommand(message, Discord);
       }
-      if (message.content === ".help admin") {
+      if (message.content === `${config.prefix}help admin`) {
         helpAdminCommand(message, Discord);
       }
-      if (message.content.startsWith(".say")){
+      if (message.content.startsWith(`${config.prefix}say`)){
         sayCommand(message, Discord)
+      }
+      //databse testing
+      if (message.content.startsWith(`${config.prefix}create`)){
+        newObj(message.author.tag, message.author.id)
+      }
+      if (message.content.startsWith(`${config.prefix}delete`)){
+        deleteObj(message.author.id)
+      }
+      if (message.content.startsWith(`${config.prefix}update`)){
+        updateObj("gej xdddd", message.author.id)
       }
     }
   });
