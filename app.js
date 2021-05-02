2728e45f78eb64aff2cf58c3ba417e4e58a68781
Gumernus <70224036+gumernus@users.noreply.github.com>
2021-05-02 13:15:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const config = require("./config.json");
 const keepAlive = require("./server");
 require("events").EventEmitter.defaultMaxListeners = 15;
 
-
 //commands
-const { helpCommand, helpNormalCommand, helpAdminCommand } = require("./commands/normal/help");
-const {joinCommand, leaveCommand} = require("./commands/normal/join_leave")
-const {sayCommand} = require("./commands/normal/easy")
-const {newObj, updateObj, deleteObj} = require("./database/database")
+const {
+  helpCommand,
+  helpNormalCommand,
+  helpAdminCommand,
+} = require("./commands/normal/help");
+const { joinCommand, leaveCommand } = require("./commands/normal/join_leave");
+const { sayCommand } = require("./commands/normal/easy");
+const { newObj, updateObj, deleteObj } = require("./database/database");
+const { voiceJoin, voiceLeave } = require("./commands/normal/voice");
 
 client.on("guildMemberAdd", (member) => {
   joinCommand(member, client, Discord);
@@ -26,9 +30,8 @@ client.on("ready", () => {
 
   client.on("message", (message) => {
     if (message.author.bot) {
-      return
-    }
-    else{
+      return;
+    } else {
       if (message.content === `${config.prefix}help`) {
         helpCommand(message, Discord);
       }
@@ -38,18 +41,24 @@ client.on("ready", () => {
       if (message.content === `${config.prefix}help admin`) {
         helpAdminCommand(message, Discord);
       }
-      if (message.content.startsWith(`${config.prefix}say`)){
-        sayCommand(message, Discord)
+      if (message.content.startsWith(`${config.prefix}say`)) {
+        sayCommand(message, Discord);
       }
       //databse testing
-      if (message.content.startsWith(`${config.prefix}create`)){
-        newObj(message.author.tag, message.author.id)
+      if (message.content.startsWith(`${config.prefix}create`)) {
+        newObj(message.author.tag, message.author.id);
       }
-      if (message.content.startsWith(`${config.prefix}delete`)){
-        deleteObj(message.author.id)
+      if (message.content.startsWith(`${config.prefix}delete`)) {
+        deleteObj(message.author.id);
       }
-      if (message.content.startsWith(`${config.prefix}update`)){
-        updateObj("gej xdddd", message.author.id)
+      if (message.content.startsWith(`${config.prefix}update`)) {
+        updateObj("gej xdddd", message.author.id);
+      }
+      if (message.content.startsWith(`${config.prefix}join`)) {
+        voiceJoin(message);
+      }
+      if (message.content.startsWith(`${config.prefix}leave`)) {
+        voiceLeave(message);
       }
     }
   });
