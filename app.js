d6c40df56f7f189604113de6045e0097f9f720c0
Gumernus <70224036+gumernus@users.noreply.github.com>
2021-05-05 18:57:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,82 +1,31 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const config = require("./config.json");
-const keepAlive = require("./server");
-require("events").EventEmitter.defaultMaxListeners = 15;
+const express = require('express');
+const server = express();
+var http = require('http');
+var fs = require('fs');
 
-//commands
-const {
-  helpCommand,
-  helpNormalCommand,
-  helpAdminCommand,
-} = require("./commands/normal/help");
-const { joinCommand, leaveCommand } = require("./commands/normal/join_leave");
-const { sayCommand } = require("./commands/normal/easy");
-const { newObj, addItem, deleteObj } = require("./database/database");
-const { voiceJoin, voiceLeave } = require("./commands/normal/voice");
-const { serverInfo } = require("./commands/normal/info");
+//colors
+let CReset = "\x1b[0m"
+let CYellow = "\x1b[33m"
+let CRed = "\x1b[31m"
 
-client.on("guildMemberAdd", (member) => {
-  joinCommand(member, client, Discord);
-});
+server.all('/', (req, res)=>{
 
-client.on("guildMemberRemove", (member) => {
-  leaveCommand(member, client, Discord);
-});
+   res.setHeader('Content-Type', 'text/html');
 
-client.on("ready", () => {
-  console.log("The client is ready!");
-  client.user.setStatus("available");
-  client.user.setActivity(config.prefix + config.status);
+   res.write('<link href="https://fonts.googleapis.com/css?family=Roboto Condensed" rel="stylesheet"> <style> body {font-family: "Roboto Condensed";font-size: 22px;} <p>Hosting Active</p>');
 
-  client.on("message", (message) => {
-    if (message.author.bot) {
-      return;
-    } else {
-      if (message.content.startsWith(`${config.prefix}help`)) {
-        helpCommand(message, Discord);
-        return;
-      }
-      if (message.content.startsWith(`${config.prefix}help normal`)) {
-        helpNormalCommand(message, Discord);
-        return;
-      }
-      if (message.content.startsWith(`${config.prefix}help admin`)) {
-        helpAdminCommand(message, Discord);
-        return;
-      }
-      if (message.content.startsWith(`${config.prefix}say`)) {
-        sayCommand(message, Discord);
-        return;
-      }
-      //databse testing
-      if (message.content.startsWith(`${config.prefix}create`)) {
-        newObj(message.author.tag, message.author.id);
-        return;
-      }
-      if (message.content.startsWith(`${config.prefix}delete`)) {
-        deleteObj(message.author.id);
-        return;
-      }
-      if (message.content.startsWith(`${config.prefix}update`)) {
-        addItem("gej xdddd", message.author.id);
-        return;
-      }
-      if (message.content.startsWith(`${config.prefix}join`)) {
-        voiceJoin(message);
-        return;
-      }
-      if (message.content.startsWith(`${config.prefix}leave`)) {
-        voiceLeave(message);
-        return;
-      }
-      if (message.content.startsWith(`${config.prefix}server info`)) {
-        serverInfo(message, Discord);
-        return;
-      }
-    }
-  });
-});
 
-keepAlive();
-client.login(config.token);
+   res.end();
+
+})
+
+
+
+function keepAlive(){
+
+   server.listen(3000, ()=>{console.log(CYellow + "$ Server is online")});
+
+}
+
+module.exports = keepAlive
+
