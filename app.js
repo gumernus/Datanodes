58f3191631421cbbd45ea226ef6819a3b4d38774
Gumernus <70224036+gumernus@users.noreply.github.com>
2021-05-07 13:34:51 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -16,10 +16,11 @@ const {
   helpAdminCommand,
 } = require("./commands/normal/help");
 const { joinCommand, leaveCommand } = require("./commands/normal/join_leave");
-const { sayCommand } = require("./commands/normal/easy");
+const { sayCommand, permaInvite, adminAlert, modAlert } = require("./commands/normal/easy");
 const { newObj, addItem, deleteObj } = require("./database/database");
 const { voiceJoin, voiceLeave } = require("./commands/normal/voice");
 const { serverInfo } = require("./commands/normal/info");
+const { Kick, Ban, Mute } = require("./commands/moderating/restrict")
 
 client.on("guildMemberAdd", (member) => {
   joinCommand(member, client, Discord);
@@ -52,33 +53,57 @@ client.on("ready", () => {
         helpAdminCommand(message, Discord);
         return;
       }
-      if (message.content.startsWith(`${config.prefix}say`)) {
+      if (message.content === `${config.prefix}say`) {
         sayCommand(message, Discord);
         return;
       }
       //databse testing
-      if (message.content.startsWith(`${config.prefix}create`)) {
+      if (message.content === `${config.prefix}create`) {
         newObj(message.author.tag, message.author.id);
         return;
       }
-      if (message.content.startsWith(`${config.prefix}delete`)) {
+      if (message.content === `${config.prefix}delete`) {
         deleteObj(message.author.id);
         return;
       }
-      if (message.content.startsWith(`${config.prefix}update`)) {
+      if (message.content === `${config.prefix}update`) {
         addItem("gej xdddd", message.author.id);
         return;
       }
-      if (message.content.startsWith(`${config.prefix}join`)) {
+      if (message.content === `${config.prefix}join`) {
         voiceJoin(message);
         return;
       }
-      if (message.content.startsWith(`${config.prefix}leave`)) {
+      if (message.content ===`${config.prefix}leave`) {
         voiceLeave(message);
         return;
       }
-      if (message.content.startsWith(`${config.prefix}server info`)) {
+      if (message.content === `${config.prefix}server info`) {
         serverInfo(message, Discord);
+        return;
+      }
+      if (message.content ===`${config.prefix}kick`) {
+        Kick(message, Discord);
+        return;
+      }
+      if (message.content === `${config.prefix}ban`) {
+        Ban(message, Discord);
+        return;
+      }
+      if (message.content === `${config.prefix}mute`) {
+        Mute(message, Discord);
+        return;
+      }
+      if (message.content === `${config.prefix}create invite`) {
+        permaInvite(message, Discord);
+        return;
+      }
+      if (message.content === `${config.prefix}admin alert`) {
+        adminAlert(message, Discord);
+        return;
+      }
+      if (message.content === `${config.prefix}mod alert`) {
+        modAlert(message, Discord);
         return;
       }
     }
