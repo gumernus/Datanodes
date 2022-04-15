2e84ab89ad29eed69c2e9bd992a0ec409630e644
gumernus <gumernusbussines@gmail.com>
2022-04-15 15:51:16 +0200

    Message sending & mentions

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include <dpp/dpp.h>
+#include <string>
+
+std::string convertToString(char* a)
+{
+	std::string s = a;
+    return s;
+}
  
 int main()
 {
 	uint64_t intents = dpp::i_default_intents | dpp::i_message_content;
-    dpp::cluster bot("", intents);
+    dpp::cluster bot("token", intents);
 	bot.on_log(dpp::utility::cout_logger());
     dpp::commandhandler command_handler(&bot);
     command_handler.add_prefix(".").add_prefix("/");
  
-    bot.on_ready([&command_handler](const dpp::ready_t &event) {
+    bot.on_ready([&command_handler, &bot](const dpp::ready_t &event) {
  
         command_handler.add_command(
             "test",
@@ -18,16 +25,19 @@ int main()
 				{"third", dpp::param_info( dpp::pt_string, true, "xd")}
 			},
  
-            [&command_handler](const std::string& command, const dpp::parameter_list_t& parameters, dpp::command_source src) {
+            [&command_handler, &bot](const std::string& command, const dpp::parameter_list_t& parameters, dpp::command_source src) {
                 std::string got_param;
                 if (!parameters.empty()) {
                     got_param = std::get<std::string>(parameters[0].second);
                 }
                 command_handler.reply(dpp::message("Pogisek -> " + got_param), src);
+				bot.message_create(dpp::message(825466224208379974,convertToString("<@") + std::to_string(src.issuer.id) + convertToString("> je gay xddd"))); // send mention to channel
+
             },"test commandík",825457047025221682
         );
  
         command_handler.register_commands();
+
 		std::cout << "BOT IS RUNNING" << "\n";
  
     });
