1fba908b033604d360ddcddda9a1bada84964465
gumernus <gumernusbussines@gmail.com>
2022-04-15 11:08:06 +0200

    First commit

--- main.cpp
+++ main.cpp
@@ -1,1 +1,29 @@
-test xddd
+#include <iostream>
+#include <cstdlib>
+#include <dpp/dpp.h>
+
+const std::string    BOT_TOKEN    = "";
+const dpp::snowflake MY_GUILD_ID  =  825457047025221682;
+
+int main() {
+    dpp::cluster bot(BOT_TOKEN);
+
+    bot.on_log(dpp::utility::cout_logger());
+
+    bot.on_interaction_create([](const dpp::interaction_create_t& event) {
+         if (event.command.get_command_name() == "ping") {
+            event.reply("Pong!");
+        }
+    });
+
+    bot.on_ready([&bot](const dpp::ready_t& event) {
+        if (dpp::run_once<struct register_bot_commands>()) {
+            bot.guild_command_create(
+                dpp::slashcommand("ping", "Ping pong!", bot.me.id),
+                MY_GUILD_ID
+            );
+        }
+    });
+
+    bot.start(false);
+}
